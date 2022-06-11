60347ea56d710aff803a385277f3f4e9dd2992b0
eufracia <ziporalicia@gmail.com>
2022-06-12 00:52:28 +0700

    Add promise for counting marah or tidak marah

--- promise.js
+++ promise.js
@@ -1,8 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
 
+const countData = (emotion, getEmotion) => {
+  return new Promise((resolve, reject) => {
+    
+    if (getEmotion === undefined) {
+      reject("Emotion is undefined");
+    } else {
+      let count = 0;
+      for (let i = 0; i < emotion.length; i++) {
+        if (getEmotion === emotion[i]["hasil"]) {
+          count++;
+        }
+      }
+      resolve(count);
+    }
+  });
+};
+
+const promiseOutput = async (getEmotion) => {
+  const countIXX = await promiseTheaterIXX().then((hasilSetelahMenonton) =>
+    countData(hasilSetelahMenonton, getEmotion)
+  );
+  const countVGC = await promiseTheaterVGC().then((hasilSetelahMenonton) =>
+    countData(hasilSetelahMenonton, getEmotion)
+  );
+  return countIXX + countVGC;
+};
 module.exports = {
   promiseOutput,
 };
